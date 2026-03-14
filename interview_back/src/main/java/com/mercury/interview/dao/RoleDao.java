dcf9418b89c666ca23f9de1ae980080cba1197e4
lingyan318 <yanling318@gmail.com>
2026-03-14 17:08:25 -0400

    clear repo

--- interview_back/src/main/java/com/mercury/interview/dao/RoleDao.java
+++
@@ -1,7 +0,0 @@
-package com.mercury.interview.dao;
-
-import com.mercury.interview.bean.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleDao extends JpaRepository<Role,Integer> {
-}
